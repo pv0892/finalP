bd14834ed5f2a66a69bc41f41d5433411854a9c8
pb389 <pranavbawa8@gmail.com>
2017-05-08 21:51:26 -0400

    task added

--- taskadd.php
+++ taskadd.php
@@ -8,42 +8,27 @@
 
 require 'model/users.php';
 
-if(isset($_POST['email'])){$f=userExists($_POST['email']);
+if(isset($_POST['taskname'])){
 
-if($f==''){
-    
- //   echo 'not found';
-    insert($_POST['email'],$_POST['password'],$_POST['firstname'],$_POST['lastname'],$_POST['birthday'],$_POST['phone'],$_POST['gender']);
-    
-}
-        else { 
-            
-            echo 'user already exists'.$f;
-        }
+
 
 }
 ?>
 <?php include 'view/header.php'; ?>
 
-<form method="post" action="register.php">
+<form method="post" action="taskadd.php">
 <table>
 <tr>
 <td>taskname:</td>
-<td><input type ="text" name="textname" class="textInput"> </td>
+<td><input type ="text" name="taskname" class="textInput"> </td>
 </tr>
 <tr>
 <td>taskdesc:</td>
 <td><input type ="text" name="taskdesc" class="textInput"> </td>
 </tr>
-<tr>
-<td>E-mail::</td>
-<td><input type ="email" name="email" class="textInput"> </td>
-</tr
 
-<tr>
-<td>Password:</td>
-<td><input type ="password" name="password" class="textInput"> </td>
-</tr>
+
+
 <tr>
 
 <tr>
@@ -52,11 +37,11 @@ if($f==''){
 </tr>
 <tr>
 <td>tasktime:</td>
-<td><input type ="text" name="tasktime" class="textInput"> </td>
+<td><input type ="time" name="tasktime" class="textInput"> </td>
 </tr>
 
 <tr>
-<td><input type="submit" name="register_btn" value="Register"></td>
+<td><input type="submit" name="register_btn" value="Done"></td>
 </tr>
 </table>
 </form>
